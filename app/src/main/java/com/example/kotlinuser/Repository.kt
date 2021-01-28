c6ecd71cad4313f277dddf5f04b9015e9ba369dc
Anthony Boffman <amarrokalboffman@gmail.com>
2021-01-28 13:55:52 -0800

    Refactor formatting to extension property of User

--- app/src/main/java/com/example/kotlinuser/Repository.kt
+++ app/src/main/java/com/example/kotlinuser/Repository.kt
@@ -2,6 +2,18 @@ package com.example.kotlinuser
 
 import java.util.*
 
+val User.formattedName: String
+    get() {
+        return if (lastName != null) {
+            if (firstName != null) {
+                "$firstName $lastName"
+            } else {
+                lastName ?: "Unknown"
+            }
+        } else {
+            firstName ?: "Unknown"
+        }
+    }
 object Repository {
     private val _users = mutableListOf<User>()
     val users: List<User>
@@ -9,19 +21,8 @@ object Repository {
 
     val formattedUserNames: List<String>
         get() {
-           return _users.map {user->
-               if(user.lastName != null){
-                   if (user.firstName != null){
-                       "${user.firstName} ${user.lastName}"
-                   } else{
-                       user.lastName ?: "Unknown"
-                   }
-               } else {
-                   user.firstName ?: "Unknown"
-               }
-           }
+            return _users.map { user -> user.formattedName }
         }
-
     // keeping the constructor private to enforce the usage of getInstance
     init {
         val user1 = User("Jane", "")
